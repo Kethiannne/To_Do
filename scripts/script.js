7bbe23b63a1dd1c13b18d8b694f241207a09f675
Kethiannne <natedadc@gmail.com>
2021-03-16 20:29:17 -0400

    Initial html/css session

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+// consts
+
+const listItemArray = [ // list of objects for each list item?
+    {
+
+    },
+    {
+
+    }
+
+]
+
+
+// functions
+
+
+// classes (if applicable)
+
+
+// listeners
+
+
+// startup
